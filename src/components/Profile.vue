95aa1a0f91d8df8e5f06c22628f77958898147b6
chrisATYF <36751474+chrisATYF@users.noreply.github.com>
2023-04-22 14:16:54 -0500

    Image Gallery created

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Container from "./Container.vue"
 import UserBar from "./UserBar.vue"
+import ImageGallery from "./ImageGallery.vue"
 
 </script>
 
@@ -15,7 +16,17 @@ import UserBar from "./UserBar.vue"
                     following: 342
                 }"
             ></UserBar>
-            <h1>IMAGEGALLERY</h1>
+            <ImageGallery :posts="[
+                {
+                    id: 1,
+                    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9UHBpQUOaUk1KncG-gATabtzPcv-Cy_chRA&usqp=CAU'
+                },
+                {
+                    id: 2,
+                    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9UHBpQUOaUk1KncG-gATabtzPcv-Cy_chRA&usqp=CAU'
+                }
+            ]"
+            ></ImageGallery>
         </div>
     </Container>
 </template>
